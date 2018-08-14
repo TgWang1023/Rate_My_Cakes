b23d9587ccc83028920734b6e7eb6466b243d6da
Tiange Wang <wtgkevin@gmail.com>
2018-08-14 01:03:38 -0700

    haha

--- server/models/cake.js
+++ server/models/cake.js
@@ -1,12 +1,12 @@
 module.exports = function(mongoose){
     const CommentSchema = new mongoose.Schema({
-        content: {type: String, default: '', required: [true, 'Comment cannot be blank!'], minlength: [5, 'Comment must be at least 5 characters long!']},
+        content: {type: String, default: '', required: [true, 'Comment cannot be blank!']},
         stars: {type: Number, default: '', required: [true, 'Rating cannot be blank!']},
     }, {timestamps: true});
     mongoose.model('Comment', CommentSchema);
 
     const CakeSchema = new mongoose.Schema({
-        baker: {type: String, default: '', required: [true, 'Baker name cannot be blank!'], minlength: [2, 'Baker name must be at least 2 characters long!']},
+        baker: {type: String, default: '', required: [true, 'Baker name cannot be blank!']},
         pic: {type: String, default: '', required: [true, 'Url cannot be blank!']},
         comments: [CommentSchema]
     }, {timestamps: true});
